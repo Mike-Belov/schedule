3002d3f156458e1e832b4258403a8ad85caacb73
Mike-Belov <149787299+Mike-Belov@users.noreply.github.com>
2026-03-07 20:27:04 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -2,42 +2,35 @@ import asyncio
 import telebot
 import requests
 from telebot.async_telebot import AsyncTeleBot  
-from telebot.types import ReplyKeyboardMarkup, KeyboardButton
+from telebot.types import ReplyKeyboardMarkup, KeyboardButton, InlineKeyboardMarkup, InlineKeyboardButton
 from bs4 import BeautifulSoup
 import DATA
-
-bot = AsyncTeleBot(DATA.TOKEN)
 
 def keyboard()->ReplyKeyboardMarkup:
     """Создание клавиатуры"""
     markup = ReplyKeyboardMarkup(resize_keyboard=True)
     # Добавляем кнопки
-    btn1 = KeyboardButton('Расписание')
-    btn2 = KeyboardButton('Информация')
+    btn1 = KeyboardButton('/schedule')
+    btn2 = KeyboardButton('/info')
     markup.add(btn1, btn2)
     return markup
 
 def parsing(soup)->str:
     """Парсит и создает ссылку для фото"""
     img = soup.find('img', alt="Ъ")
-
     return DATA.URL2+img.get('src')
-
-async def restart_task():
-    """Асинхронная задача перезагрузки"""
-    asyncio.run(main())
     
 async def periodic_restart():
     """Периодическая перезагрузка"""
     while True:
         await asyncio.sleep(3600)
-        await restart_task()
+        asyncio.run(main())
 
 async def main():
     """Основная асинхронная функция"""
     # Запускаем задачу перезагрузки  
     asyncio.create_task(periodic_restart())
-    
+
     # Запускаем бота  
     bot=telebot.TeleBot(DATA.TOKEN)
 
@@ -59,24 +52,24 @@ async def main():
         markup=keyboard()
         bot.send_message(message.chat.id,"Привет {0.first_name}! Хочешь узнать расписание нажми кнопку 'Расписание'".format(message.from_user), reply_markup=markup)
 
-    @bot.message_handler(content_types=['text'])
+    @bot.message_handler(commands=['schedule'])
     def schedule(message):
-        markup=keyboard()
-        if message.text == "Расписание":
-            bot.send_photo(message.chat.id, parsing(soup), caption="Расписание")   
-        elif message.text == "Информация":
-             bot.send_message(message.chat.id, "Вся информация берется с сайта " + 
-                              f"https://gimn3-53.gosuslugi.ru. Телеграмм  bot с открытым исходным кодом. Ссылка на GitHub " + 
-                              f"https://github.com/Mike-Belov/schedule. По всем вопросам @MikeBelovv", reply_markup=markup)  
-        else:
-            bot.send_message(message.chat.id, "Бот не понял вас попробуйте еще раз")
+        markup = keyboard()
+        bot.send_photo(message.chat.id, parsing(soup), caption="Расписание", reply_markup=markup) 
+        bot.delete_message(message.chat.id, message.message_id) 
+    @bot.message_handler(commands=['info'])
+    def info(message):
+        markup = keyboard()
+        bot.send_message(message.chat.id, "Вся информация берется с сайта " + 
+                          f"https://gimn3-53.gosuslugi.ru. Телеграмм  bot с открытым исходным кодом. Ссылка на GitHub " + 
+                          f"https://github.com/Mike-Belov/schedule. По всем вопросам @MikeBelovv", reply_markup=markup)  
+        bot.delete_message(message.chat.id, message.message_id)
 
     while True:
         try:
-            await bot.polling(non_stop=True, timeout=123)
+            await bot.infinity_polling(timeout=10, long_polling_timeout = 5)
         except Exception as e:
             print(e)  
 
 if __name__ == "__main__":
-
     asyncio.run(main())
